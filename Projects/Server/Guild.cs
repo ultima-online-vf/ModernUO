67601773ac789952b52b08a8daf5fdff62e2d94d
Kamron Batman <3953314+kamronbatman@users.noreply.github.com>
2021-03-14 01:39:47 -0800

    More cleanup

--- Projects/Server/Guild.cs
+++ Projects/Server/Guild.cs
@@ -1,6 +1,20 @@
+/*************************************************************************
+ * ModernUO                                                              *
+ * Copyright (C) 2019-2021 - ModernUO Development Team                   *
+ * Email: hi@modernuo.com                                                *
+ * File: Guild.cs                                                        *
+ *                                                                       *
+ * This program is free software: you can redistribute it and/or modify  *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *************************************************************************/
+
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 
 namespace Server.Guilds
 {
@@ -49,18 +63,11 @@ namespace Server.Guilds
         [CommandProperty(AccessLevel.Counselor)]
         public Serial Serial { get; }
 
+        long ISerializable.SavePosition { get; set; }
+
         BufferWriter ISerializable.SaveBuffer { get; set; }
 
         public int TypeRef { get; private set; }
-
-        private long _savePosition;
-        long ISerializable.SavePosition
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => _savePosition;
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => _savePosition = value;
-        }
 
         public abstract void Serialize(IGenericWriter writer);
         public abstract void Deserialize(IGenericReader reader);
